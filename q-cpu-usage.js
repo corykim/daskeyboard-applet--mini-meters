dd8d93cb1d4be5d5a0d8adf57449b9c7597b7e6f
Cory Kim <github@cory.kim>
2018-10-12 15:26:43 -0500

    feat(geometry): Add geometry settings to qConfig

--- q-cpu-usage.js
+++ q-cpu-usage.js
@@ -4,8 +4,6 @@ const os = require('os-utils');
 
 const q = require('daskeyboard-applet');
 
-// Coordinates of keys [`,1,2,3,4,5,6,7,8,9]
-const zoneIds = ['1,1', '2,1', '3,1', '4,1', '5,1', '6,1', '7,1', '8,1', '9,1', '10,1'];
 // Color associated to each coordinate
 const colors = ['#00FF00', '#00FF00', '#00FF00', '#00FF00', '#FFFF00', '#FFFF00', '#FF0000',
   '#FF0000', '#FF0000', '#FF0000'];
@@ -30,7 +28,7 @@ class CpuUsage extends q.DesktopApp {
   /** get the cpu usage percentage  */
   getCpuUsage() {
     os.cpuUsage((v) => {
-      const numberOfKeys = zoneIds.length;
+      const numberOfKeys = 10;
       // multiply the cpu percentage by the number total of keys 
       const numberOfKeysToLight = Math.round(numberOfKeys * v) + 1;
       let points = [];
